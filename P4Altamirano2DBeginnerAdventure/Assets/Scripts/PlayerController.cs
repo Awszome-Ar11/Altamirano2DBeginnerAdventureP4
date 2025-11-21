bec0b914c25fe4dccee6bbd45ff5b9b3d0eafdf8
Awszome-Ar11 <743241@student.dusd.net>
2025-11-21 11:22:50 -0800

    added collider
    added collider and fixed scripts

--- P4Altamirano2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P4Altamirano2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //Variables related to player character movement
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
 
@@ -17,6 +22,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
         float horizontal = 0.0f;
         if (Keyboard.current.leftArrowKey.isPressed)
         {
@@ -43,5 +50,11 @@ public class PlayerController : MonoBehaviour
         position.x = position.x + 0.01f * horizontal;
         position.y = position.y + 0.01f * vertical;
         transform.position = position;
+
+        void FixedUpdate()
+        {
+            Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+            rigidbody2d.MovePosition(position);
+        }
     }
 }
